eb157c71c326befc3b0a449aaae5bb254087befb
neil tarar <neiltarar@gmail.com>
2023-04-29 17:56:37 +1000

    Initial scaffold of Django, simple user DB and a TypeScript React app build.

--- brew_book/brew_book/settings.py
+++ brew_book/brew_book/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&w368)&vk-q*l*x!3xt=nmy5bg1&wna7n^2xj83^m%oee**^%r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
