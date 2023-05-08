85b1a4d09913ec5dbecd2fdab6a83d3eb9bddb8e
neil tarar <neiltarar@gmail.com>
2023-05-08 23:23:51 +1000

    server static files via Django

--- brew_book/brew_book/settings.py
+++ brew_book/brew_book/settings.py
@@ -129,8 +129,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'frontend/build')]
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Add the build folder to STATICFILES_DIRS
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/build/static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- brew_book/brew_book/urls.py
+++ brew_book/brew_book/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('api/beer_notes/', include('beer_notes.urls'))
+    path('api/beer_notes/', include('beer_notes.urls')),
+    # Serve the React app's index.html as a catch-all
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
